a120558d437a5b163b399b05caf3ec416c2d0b19
marco-streng <marcostreng@me.com>
2023-01-27 11:19:01 +0100

    chore: reduce package size

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { awscdk } = require("projen");
+const { awscdk, javascript } = require("projen");
 
 const project = new awscdk.AwsCdkConstructLibrary({
   author: "Marco Streng",
@@ -12,19 +12,23 @@ const project = new awscdk.AwsCdkConstructLibrary({
     "CloudWatch",
     "MultiRegion",
   ],
+  packageManager: javascript.NodePackageManager.YARN,
   license: "MIT",
   cdkVersion: "2.62.1",
   defaultReleaseBranch: "main",
   repositoryUrl:
     "https://github.com/marco-streng/patch-log-groups-multi-region.git",
   prettier: true,
-  bundledDeps: [
+  devDeps: [
+    "@types/aws-lambda",
     "@aws-sdk/client-cloudwatch-logs",
     "@aws-lambda-powertools/logger",
   ],
-  devDeps: ["@types/aws-lambda"],
   lambdaOptions: {
     runtime: awscdk.LambdaRuntime.NODEJS_18_X,
+    bundlingOptions: {
+      externals: ["@aws-sdk/*"],
+    },
   },
   releaseToNpm: true,
 });
